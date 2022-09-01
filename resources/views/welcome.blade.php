f6f7c1b18d0416ae46d47ba03b6b9619461773e7
Diego Del bianco <diego@delbianco.emp.br>
2022-09-01 02:52:36 -0300

    Alterado a pagina inicial do site principal da kiim

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,119 +21,164 @@
             /*! normalize.css v8.0.1 | MIT License | github.com/necolas/normalize.css */html{line-height:1.15;-webkit-text-size-adjust:100%}body{margin:0}a{background-color:transparent}[hidden]{display:none}html{font-family:system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji;line-height:1.5}*,:after,:before{box-sizing:border-box;border:0 solid #e2e8f0}a{color:inherit;text-decoration:inherit}svg,video{display:block;vertical-align:middle}video{max-width:100%;height:auto}.bg-white{--bg-opacity:1;background-color:#fff;background-color:rgba(255,255,255,var(--bg-opacity))}.bg-gray-100{--bg-opacity:1;background-color:#f7fafc;background-color:rgba(247,250,252,var(--bg-opacity))}.border-gray-200{--border-opacity:1;border-color:#edf2f7;border-color:rgba(237,242,247,var(--border-opacity))}.border-t{border-top-width:1px}.flex{display:flex}.grid{display:grid}.hidden{display:none}.items-center{align-items:center}.justify-center{justify-content:center}.font-semibold{font-weight:600}.h-5{height:1.25rem}.h-8{height:2rem}.h-16{height:4rem}.text-sm{font-size:.875rem}.text-lg{font-size:1.125rem}.leading-7{line-height:1.75rem}.mx-auto{margin-left:auto;margin-right:auto}.ml-1{margin-left:.25rem}.mt-2{margin-top:.5rem}.mr-2{margin-right:.5rem}.ml-2{margin-left:.5rem}.mt-4{margin-top:1rem}.ml-4{margin-left:1rem}.mt-8{margin-top:2rem}.ml-12{margin-left:3rem}.-mt-px{margin-top:-1px}.max-w-6xl{max-width:72rem}.min-h-screen{min-height:100vh}.overflow-hidden{overflow:hidden}.p-6{padding:1.5rem}.py-4{padding-top:1rem;padding-bottom:1rem}.px-6{padding-left:1.5rem;padding-right:1.5rem}.pt-8{padding-top:2rem}.fixed{position:fixed}.relative{position:relative}.top-0{top:0}.right-0{right:0}.shadow{box-shadow:0 1px 3px 0 rgba(0,0,0,.1),0 1px 2px 0 rgba(0,0,0,.06)}.text-center{text-align:center}.text-gray-200{--text-opacity:1;color:#edf2f7;color:rgba(237,242,247,var(--text-opacity))}.text-gray-300{--text-opacity:1;color:#e2e8f0;color:rgba(226,232,240,var(--text-opacity))}.text-gray-400{--text-opacity:1;color:#cbd5e0;color:rgba(203,213,224,var(--text-opacity))}.text-gray-500{--text-opacity:1;color:#a0aec0;color:rgba(160,174,192,var(--text-opacity))}.text-gray-600{--text-opacity:1;color:#718096;color:rgba(113,128,150,var(--text-opacity))}.text-gray-700{--text-opacity:1;color:#4a5568;color:rgba(74,85,104,var(--text-opacity))}.text-gray-900{--text-opacity:1;color:#1a202c;color:rgba(26,32,44,var(--text-opacity))}.underline{text-decoration:underline}.antialiased{-webkit-font-smoothing:antialiased;-moz-osx-font-smoothing:grayscale}.w-5{width:1.25rem}.w-8{width:2rem}.w-auto{width:auto}.grid-cols-1{grid-template-columns:repeat(1,minmax(0,1fr))}@media (min-width:640px){.sm\:rounded-lg{border-radius:.5rem}.sm\:block{display:block}.sm\:items-center{align-items:center}.sm\:justify-start{justify-content:flex-start}.sm\:justify-between{justify-content:space-between}.sm\:h-20{height:5rem}.sm\:ml-0{margin-left:0}.sm\:px-6{padding-left:1.5rem;padding-right:1.5rem}.sm\:pt-0{padding-top:0}.sm\:text-left{text-align:left}.sm\:text-right{text-align:right}}@media (min-width:768px){.md\:border-t-0{border-top-width:0}.md\:border-l{border-left-width:1px}.md\:grid-cols-2{grid-template-columns:repeat(2,minmax(0,1fr))}}@media (min-width:1024px){.lg\:px-8{padding-left:2rem;padding-right:2rem}}@media (prefers-color-scheme:dark){.dark\:bg-gray-800{--bg-opacity:1;background-color:#2d3748;background-color:rgba(45,55,72,var(--bg-opacity))}.dark\:bg-gray-900{--bg-opacity:1;background-color:#1a202c;background-color:rgba(26,32,44,var(--bg-opacity))}.dark\:border-gray-700{--border-opacity:1;border-color:#4a5568;border-color:rgba(74,85,104,var(--border-opacity))}.dark\:text-white{--text-opacity:1;color:#fff;color:rgba(255,255,255,var(--text-opacity))}.dark\:text-gray-400{--text-opacity:1;color:#cbd5e0;color:rgba(203,213,224,var(--text-opacity))}.dark\:text-gray-500{--tw-text-opacity:1;color:#6b7280;color:rgba(107,114,128,var(--tw-text-opacity))}}
         </style>
 
+        <script src="https://kit.fontawesome.com/3018d5f091.js" crossorigin="anonymous"></script>
+        <script src="{{ asset('js/principal/jquery-3.6.0.min.js') }}"></script>
+        <link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.min.css') }}">
         <style>
             body {
                 font-family: 'Nunito', sans-serif;
+                background-color: #f7fafc;
+            }
+            .nav-login{
+                background-color: #444;
+            }
+            .nav-login a{
+                color: #fff;
+                text-decoration: none;
+            }
+            .nav-login a:hover{
+                color: #ddd;
+            }
+            .session-principal h1{
+                display: initial;
+                line-height: 59px;
+                text-shadow: 1px 1px 1px #333;
+                background-color: rgba(255, 255, 255, 0.6)
+            }
+            .session-principal .btn{
+                font-size: 20px;
+            }
+            .section-princpal{
+                background: url({{asset('images/principal/back-session-1.jpg')}});
+                background-size: cover;
+                background-position: center;
+                background-attachment: fixed;
+            }
+            .footer{
+                background-color: #444;   
+            }
+            .footer a, .footer p, .footer ul, .footer li{
+                color: #fff;
+            }
+            .card li.title{
+                font-weight:bold;
+                background-color: #ccc;
+            }
+            .card li.no{
+                background: #ffc3c3;
+                text-decoration: line-through;
+            }
+            .card{
+                margin-left: auto;
+                margin-right: auto;
+            }
+            @media (min-width:600px){
+                .card.card-f{
+                    margin-right: 0px;
+                }
+                .card.card-s{
+                    margin-left: 0px;
+                }
             }
         </style>
 
     </head>
     <body class="antialiased body-site">
-        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-            @if (Route::has('login'))
-                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-                    @auth
-                        <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Painel</a>
-                    @else
-                        <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Entrar</a>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Criar conta</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
+        <div class="container-fluid nav-login">
+            <div class="container">
+                <div class="row">
+                    <div class="p-2 col-md-12 text-right">
+                        @auth
+                            <a href="{{ url('/dashboard') }}" class="p-2 text-sm text-gray-700 dark:text-gray-500 underline">Painel</a>
+                        @else
+                            <a href="{{ route('login') }}" class="p-2 text-sm text-gray-700 dark:text-gray-500 underline">Entrar</a>
 
-            <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
-                <div class="logo flex justify-center pt-8 sm:justify-start sm:pt-0">
-                    <img id="top-logo" src="{{asset('images/principal/brand/2c.png')}}" alt="">
-                </div>
-
-                <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
-                    <div class="grid grid-cols-1 md:grid-cols-2">
-                        <div class="p-6">
-                            <div class="flex items-center">
-                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"></path></svg>
-                                <div class="ml-4 text-lg leading-7 font-semibold"><a href="https://laravel.com/docs" class="underline text-gray-900 dark:text-white">Crie seu site Grátis</a></div>
-                            </div>
-
-                            <div class="ml-12">
-                                <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                    Crie seu site agora grátis, e ja comece a divulgar seus imóveis de forma profissional.<br />
-                                    Nosso plano gratuito inclui um dominio .kiim.com.br personalizado para você.
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-t-0 md:border-l">
-                            <div class="flex items-center">
-                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z"></path><path d="M15 13a3 3 0 11-6 0 3 3 0 016 0z"></path></svg>
-                                <div class="ml-4 text-lg leading-7 font-semibold"><a href="https://laracasts.com" class="underline text-gray-900 dark:text-white">Veja nossa lista de Ferramentas</a></div>
-                            </div>
-
-                            <div class="ml-12">
-                                <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                    Estamos sempre densenvolvendo novas ferramentas para te ajudar com o marketing digital, e a gerenciar seus imóveis. Faça sua conta e veja nossa lista de ferramentas que vai te fazer ir para um outro nível.
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="p-6 border-t border-gray-200 dark:border-gray-700">
-                            <div class="flex items-center">
-                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"></path></svg>
-                                <div class="ml-4 text-lg leading-7 font-semibold"><a href="https://laravel-news.com/" class="underline text-gray-900 dark:text-white">Divulgue seus imóveis</a></div>
-                            </div>
-
-                            <div class="ml-12">
-                                <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                    É facil, e fica bem mais profissional para apresentar uma lista dos seus imóveis para seus cliente, ou compartilhar um link seu, nas redes sociais.
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-l">
-                            <div class="flex items-center">
-                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-                                <div class="ml-4 text-lg leading-7 font-semibold text-gray-900 dark:text-white">Vire Kiim PRO</div>
-                            </div>
-
-
-                            <div class="ml-12">
-                                <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                    Tenha acesso total a todo nosso sistema, e funções, que vão te fazer subir de nivel. Por um precinho SUPER justo.
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="flex justify-center mt-4 sm:items-center sm:justify-between">
-                    <!--div class="text-center text-sm text-gray-500 sm:text-left">
-                        <div class="flex items-center">
-                            <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor" class="-mt-px w-5 h-5 text-gray-400">
-                                <path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path>
-                            </svg>
-
-                            <a href="https://laravel.bigcartel.com" class="ml-1 underline">
-                                Shop
-                            </a>
-
-                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="ml-4 -mt-px w-5 h-5 text-gray-400">
-                                <path d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path>
-                            </svg>
-
-                            <a href="https://github.com/sponsors/taylorotwell" class="ml-1 underline">
-                                Sponsor
-                            </a>
-                        </div>
-                    </div-->
-
-                    <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
-                        Kim v0.0001
+                            @if (Route::has('register'))
+                                <a href="{{ route('register') }}" class="p-2 ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Criar conta</a>
+                            @endif
+                        @endauth
                     </div>
                 </div>
             </div>
         </div>
-        <script src=""></script>
+
+        <header class="container">
+            <div class="row mb-3 pt-2">
+                <div class="col-md-12 text-center">
+                    <img id="top-logo" src="{{asset('images/principal/brand/2c.png')}}" alt="">
+                </div>
+            </div>
+        </header>
+
+        <section class="container-fluid section-princpal pt-5 pb-5">
+            <div class="container">
+                <div class="row session-principal">
+                    <div class="col-md-12 pb-3">
+                        <h1>Crie seu site Imbiliário <br> <b>Grátis</b> Agora</h1>
+                    </div>
+                    <div class="col-md-12 pt-2">
+                        <a href="{{ route('register') }}" class="btn btn-primary"> Criar conta Grátis </a>
+                        <p>Para corretores <br> E imobiliárias</p>
+                    </div>
+                </div>
+            </div>
+        </section>  
+
+        <section class="container p-3 section-planos">
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <h2>Planos</h2>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6"> 
+                    <div class="card card-f" style="width: 22rem;">
+                      <ul class="list-group list-group-flush">
+                        <li class="list-group-item title">Gratis</li>
+                        <li class="list-group-item ok">Até 2 sites</li>
+                        <li class="list-group-item ok">Imóveis imitados</li>
+                        <li class="list-group-item ok">Até 1 Lead Page por Imóvel</li>
+                        <li class="list-group-item ok">Colaboradores ilimitados para cada site</li>
+                        <li class="list-group-item ok">Compartilhamento de imóveis com outros sites (Sites de parceiros e corretores)</li>
+                        <li class="list-group-item ok">Farramenta para criar posts dos imóveis</li>
+                        <li class="list-group-item no">Site sem nossa marca fixada na tela</li>
+
+                      </ul>
+                    </div>
+                </div>
+                <div class="col-md-6"> 
+                    <div class="card card-s" style="width: 22rem;">
+                      <ul class="list-group list-group-flush">
+                        <li class="list-group-item title">1 Premium</li>
+                        <li class="list-group-item ok">Até <b>3</b> sites</li>
+                        <li class="list-group-item ok">Imóveis imitados</li>
+                        <li class="list-group-item ok">Lead Page <b>Imilitadas</b> por Imóvel</li>
+                        <li class="list-group-item ok">Colaboradores ilimitados para cada site</li>
+                        <li class="list-group-item ok">Compartilhamento de imóveis com outros sites (Sites de parceiros e corretores)</li>
+                        <li class="list-group-item ok">Farramenta para criar posts dos imóveis</li>
+                        <li class="list-group-item ok">Site sem nossa marca fixada na tela</li>
+                      </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="container pt-3 pb-5 mt-4 text-center">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2>Cadastre-se Agora</h2>
+                    <a href="{{route('register')}}" class="btn btn-success">Cadastrar</a>
+                </div>
+            </div>
+        </section>
+        <footer class="container-fluid footer pt-3 pb-2 mt-4">
+            <section class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <p>Desenvolvido por <a href="https://delbianco.emp.br/">Diego Del Bianco</a></p>
+                    </div>
+                </div>
+            </section>
+        </footer>
     </body>
 </html>
